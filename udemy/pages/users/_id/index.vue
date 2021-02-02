f3d22c67a0df0b4cf381a4784f5170f66dde9766
latif_abdurrahman <latif.abdurrahman@mail.ugm.ac.id>
2021-02-02 18:09:02 +0700

    validate route id

--- udemy/pages/users/_id/index.vue
+++ udemy/pages/users/_id/index.vue
@@ -1,3 +1,11 @@
 <template>
     <p>This single user ID: {{ $route.params.id }}</p>
 </template>
+
+<script>
+export default {
+    validate(data){
+        return data.params.id != 'text'
+    }
+}
+</script>
